5e1c81ee551363b5c5c428c2cb382045a8124c6f
malenethoming <malene.thoming@gmail.com>
2016-10-02 21:16:27 +0200

    Prøver at få databasen til at virke

--- Login.php
+++ Login.php
@@ -1,3 +1,9 @@
+<?php
+if(!empty($_POST['email']) && !empty($_POST ['password']));
+
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -8,6 +14,8 @@
 
 <body>
 <h1>Log in</h1>
+<span> or <a href="register.php">register here</a></span>
+
 <form action="login.php" metod="POST">
 <input type="text" placeholder="Enter your email" name="email">
 <input type="password" placeholder="and password" name="password">
--- register.php
+++ register.php
@@ -1,3 +1,9 @@
+<?php
+if(!empty($_POST['email']) && !empty($_POST ['password']));
+// Enter the new user in the database
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -8,5 +14,16 @@
 
 <body>
 <h1>Register</h1>
+<span> or <a href="login.php">login here</a></span>
+<form action="register.php" metod="POST">
+<input type="text" placeholder="Enter your email" name="email">
+<input type="password" placeholder="and password" name="password">
+<input type="submit">
+
+
+
+
+
+</form>
 </body>
 </html>
